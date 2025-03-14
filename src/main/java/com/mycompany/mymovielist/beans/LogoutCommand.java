1e437f69e72ebc2fb5114fc5ec76da418fc4d9e6
Kiran Neupane <kiran10neupane@gmail.com>
2025-03-14 09:36:13 +0545

    added availablemovies.xhtml

--- src/main/java/com/mycompany/mymovielist/beans/LogoutCommand.java
+++ src/main/java/com/mycompany/mymovielist/beans/LogoutCommand.java
@@ -9,18 +9,18 @@ import com.mycompany.mymovielist.view.*;
  *
  * @author kiran
  */
-public class LogoutCommand implements Command {
-    private final UI ui;
-    private final NavigationManager navManager;
-    
-    public LogoutCommand(UI ui, NavigationManager navManager) {
-        this.ui = ui;
-        this.navManager = navManager;
-    }
-    
-    @Override
-    public void execute() {
-        navManager.exit();
-        ui.logout();
-    }
-}
+//public class LogoutCommand implements Command {
+//    private final UI ui;
+//    private final NavigationManager navManager;
+//    
+//    public LogoutCommand(UI ui, NavigationManager navManager) {
+//        this.ui = ui;
+//        this.navManager = navManager;
+//    }
+//    
+//    @Override
+//    public void execute() {
+//        navManager.exit();
+//        ui.logout();
+//    }
+//}
